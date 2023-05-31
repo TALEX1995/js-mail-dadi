3452f7a26f5a8638ebed20b482f005f046fa97e3
TALEX1995 <tanase.alex@libero.it>
2023-05-31 16:12:34 +0200

    Added varaible for message into DOM+hotfix comment

--- script/script.es1.js
+++ script/script.es1.js
@@ -1,13 +1,18 @@
 console.log('JS OK');
 
 
-// Prendo gli elementi che mi serviranno dal DOM
+// Recover element from DOM
 
 const email = document.getElementById('e-mail');
 const access = document.getElementById('access');
 const messageAccessPlaceholder = document.getElementById('message-access');
 
-// Creazione array
+
+// Create Array
 
 const emailArray = ['alextanase95@gmail.com', 'pippo@gmail.com', 'achille@gmail.com', 'spartacus#gmail.com', 'mario@gmail.com', 'drstrange@gmail.com']
 
+
+// Message into DOM
+
+const messageAccess = 'Mi dispiace non è in lista'
