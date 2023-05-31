65a71a2403772b1f77c094576fddfec5f225bbef
TALEX1995 <tanase.alex@libero.it>
2023-05-31 17:27:02 +0200

    Added random number

--- script/script.es2.js
+++ script/script.es2.js
@@ -1,1 +1,19 @@
 console.log('JS OK');
+
+// Result
+
+let result = 'Parità'
+
+
+// Variable for min and max
+const min = 1
+const max = 6
+
+
+// Random Number
+
+const randomNumberPc = Math.floor(Math.random() * max) + 1;
+
+const randomNumberPlayer = Math.floor(Math.random() * max) + 1;
+console.log(randomNumberPc, randomNumberPlayer);
+
