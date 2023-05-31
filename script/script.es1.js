d85de18bb68fbcbab16f115a43b143328b6180ba
TALEX1995 <tanase.alex@libero.it>
2023-05-31 16:09:52 +0200

    Added HTML elements from DOM and Array

--- script/script.es1.js
+++ script/script.es1.js
@@ -1,1 +1,13 @@
 console.log('JS OK');
+
+
+// Prendo gli elementi che mi serviranno dal DOM
+
+const email = document.getElementById('e-mail');
+const access = document.getElementById('access');
+const messageAccessPlaceholder = document.getElementById('message-access');
+
+// Creazione array
+
+const emailArray = ['alextanase95@gmail.com', 'pippo@gmail.com', 'achille@gmail.com', 'spartacus#gmail.com', 'mario@gmail.com', 'drstrange@gmail.com']
+
