7c3efa7c7619f206ae2e6faa5cce494eced34bd5
TALEX1995 <tanase.alex@libero.it>
2023-05-31 17:29:47 +0200

    Added condition

--- script/script.es2.js
+++ script/script.es2.js
@@ -17,3 +17,12 @@ const randomNumberPc = Math.floor(Math.random() * max) + 1;
 const randomNumberPlayer = Math.floor(Math.random() * max) + 1;
 console.log(randomNumberPc, randomNumberPlayer);
 
+// Condition
+
+if (randomNumberPc > randomNumberPlayer) {
+    result = 'Il computer ha vinto'
+}   else if (randomNumberPc < randomNumberPlayer) {
+    result = 'Hai Vinto'
+}
+
+console.log(result)
