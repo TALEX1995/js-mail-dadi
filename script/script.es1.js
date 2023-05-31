f8b041680f4dee342c16629fbed4792a13bf93de
TALEX1995 <tanase.alex@libero.it>
2023-05-31 16:32:57 +0200

    Added condition

--- script/script.es1.js
+++ script/script.es1.js
@@ -3,7 +3,7 @@ console.log('JS OK');
 
 // Recover element from DOM
 
-const email = document.getElementById('e-mail');
+const emailInput = document.getElementById('e-mail');
 const access = document.getElementById('access');
 const messageAccessPlaceholder = document.getElementById('message-access');
 
@@ -15,7 +15,7 @@ const emailArray = ['alextanase95@gmail.com', 'pippo@gmail.com', 'achille@gmail.
 
 // Message into DOM
 
-const messageAccess = 'Mi dispiace non è in lista'
+let messageAccess = 'Mi dispiace non è in lista'
 
 
 // Function on click Listener
@@ -25,9 +25,17 @@ access.addEventListener('click', function() {
     // Cicle
     for (let i = 0; i < emailArray.length; i++) {
         
+        const emailControl = emailArray[i];
+        const emailUser = emailInput.value;
         
+        if (emailUser === emailControl) {
+            messageAccess = 'Prego può entrare'
+        }   
     }
 
+    console.log(messageAccess);
 })
+
+
 
 
